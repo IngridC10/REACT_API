575a28e79d4511ef5d09e417c50f22852f836908
Ingrid <ingridcalzada@MacBook-Pro-de-Ingrid-2.local>
2023-12-22 15:23:27 -0500

    se hizo el search

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SearchComponent = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
 
-  
+  const handleSearch = () => {
+    navigate(`/detail/${searchTerm}`);
+  };
+
   return (
     <div className="search">
-    <input type="text" placeholder="Buscar..." />
-     <button>
-        Buscar
-     </button>
-      
+      <input
+        type="text"
+        placeholder="Buscar..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Buscar</button>
     </div>
-  )
-}
+  );
+};
 
-export default SearchComponent
+export default SearchComponent;
--- src/router/RouterPrincipal.js
+++ src/router/RouterPrincipal.js
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import DetailsPokemon from '../components/pages/DetailsPokemon';
 import HomePrincipal from '../components/pages/HomePrincipal';
 
+
 const RouterPrincipal = () => {
   return (
     <Routes>
       <Route path="/home" element={<HomePrincipal />} />
       <Route path="/detail/:id" element={<DetailsPokemon />} />
+      <Route path="/search/:term" element={<DetailsPokemon />} /> 
     </Routes>
   );
 };
