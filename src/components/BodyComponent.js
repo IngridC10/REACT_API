010f1d512efef384a477e6ddaa40cd261010d39f
Ingrid <ingridcalzada@MacBook-Pro-de-Ingrid-2.local>
2023-12-22 22:03:00 -0500

    se hizo la paginacion

--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -1,29 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-
 const BodyComponent = () => {
-const[pokemonList,setpokemonList]=useState([])
-
-
+  const [pokemonList, setPokemonList] = useState([]);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
       .then(response => response.json())
       .then(data => {
-        setpokemonList(data.results);
+        setPokemonList(data.results);
       })
       .catch(error => console.log("Error al obtener los datos", error));
   }, []);
 
   return (
-    
     <div className='pokemon'>
       <h1>Listado de pokemones</h1>
       <ul>
         {pokemonList.map((pokemon, index) => (
           <li key={index}>
-            <Link to={`/detail/${index + 1}`}>{pokemon.name}</Link>
+            <Link to={`/detail/${index + 1}`}>{index + 1}. {pokemon.name}</Link>
           </li>
         ))}
       </ul>
